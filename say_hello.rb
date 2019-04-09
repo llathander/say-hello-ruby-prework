2dd33b743cc70595ba8ddab08b08f719270dfbce
Adrian Samaniego <llathander@gmail.com>
2019-04-09 16:41:39 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,8 +2,8 @@ def say_hello(name)
 puts "Hello Kent Beck!"
 end
 
- def say_hello(name="Ruby Programmer!")
-  puts ("Hello #{name}")
+ def say_hello(name="Ruby Programmer")
+  puts ("Hello #{name}!")
 end 
 
 say_hello()
